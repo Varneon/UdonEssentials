f499f23118022d279b8d6c09ebc706f4f901b10a
Varneon <me@varneon.com>
2021-07-18 06:36:36 -0700

    Add readme and example for UdonConsole
    -Added readme
    -Added example script for copying the necessary methods and variables in order to use the UdonConsole for logging your own UdonBehaviours

---
+++ Assets/Varneon/Udon Prefabs/Runtime Tools/Console/Examples/UdonConsoleUsageExample.cs
@@ -0,0 +1,47 @@
+﻿#if !UDONSHARP_COMPILER && UNITY_EDITOR
+
+using UdonSharp;
+using UnityEngine;
+
+public class UdonConsoleUsageExample : UdonSharpBehaviour
+{
+    #region VARIABLES
+    [SerializeField]
+    private Varneon.UdonPrefabs.RuntimeTools.UdonConsole Console;
+
+    //Setting up a custom prefix for the UdonSharp class makes it easy to see from which UdonBehaviour the log entries are coming from
+    private const string LOG_PREFIX = "[<color=Cyan>YOURCLASSNAMEHERE</color>]: ";
+    #endregion
+
+    #region LOGGING EXAMPLE
+    private void Start()
+    {
+        //Instead of Debug.Log or other variants you can now use the proxies below
+        Log("This is how you can log normal messages");
+        LogWarning("You can use LogWarning for logging warnings");
+        LogError("If something goes wrong, you can also use LogError");
+    }
+    #endregion
+
+    #region PROXY METHODS
+    private void Log(string message)
+    {
+        Debug.Log(message = $"{LOG_PREFIX} {message}");
+        if (Console) { Console._Log(message); }
+    }
+
+    private void LogWarning(string message)
+    {
+        Debug.LogWarning(message = $"{LOG_PREFIX} {message}");
+        if (Console) { Console._LogWarning(message); }
+    }
+
+    private void LogError(string message)
+    {
+        Debug.LogError(message = $"{LOG_PREFIX} {message}");
+        if (Console) { Console._LogError(message); }
+    }
+    #endregion
+}
+
+#endif
---
+++ Assets/Varneon/Udon Prefabs/Runtime Tools/Console/README.md
@@ -0,0 +1,14 @@
+Varneon's UdonEssentials - Udon Console
+
+This prefab is an in-game console based on the one found in Unity Editor and is made for debugging UdonBehaviours during runtime either for development purposes or for showing behind-the-scene data to a user in a world
+
+The console has public methods that you can access from your scripts to log different types of messages
+
+
+How to use the console:
+
+1: Copy and paste the necessary variables and proxy methods provided in the example script located in the "Examples" folder into your UdonSharpBehaviour
+
+2: Use the log proxy methods in any part of your code by using to send messages to the console if one exists and is assigned
+
+3: Place the "Console" prefab into your scene and assign it to the UdonBehaviours you want to use to send messages to it 
