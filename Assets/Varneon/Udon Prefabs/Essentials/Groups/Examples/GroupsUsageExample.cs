192acb032f1a53cfa18cebdd36c1a50bcdf968e5
Varneon <me@varneon.com>
2021-11-29 20:40:32 -0800

    Add groups core
    -Added groups core for establishing in-game groups with lists of usernames, icons and arguments which can be accessed from Udon during runtime

---
+++ Assets/Varneon/Udon Prefabs/Essentials/Groups/Examples/GroupsUsageExample.cs
@@ -0,0 +1,32 @@
+﻿#if !UDONSHARP_COMPILER && UNITY_EDITOR
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class GroupsUsageExample : UdonSharpBehaviour
+{
+    [SerializeField]
+    private Varneon.UdonPrefabs.Essentials.Groups groups;
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        string displayName = player.displayName;
+
+        // Get the group indices of the player based on the displayName of the player
+        int[] groupIndices = groups._GetGroupIndicesOfPlayer(displayName);
+
+        // Iterate through every group that the player is part of
+        foreach (int index in groupIndices)
+        {
+            // The following information can be fetched from groups based on the index of the group
+            string groupName = groups._GetGroupName(index);
+            Sprite groupIcon = groups._GetGroupIcon(index);
+            string groupArgs = groups._GetGroupArguments(index);
+
+            Debug.Log($"{displayName} <color=silver>Is part of group:</color> {groupName}\n<color=silver>Group icon:</color> {(groupIcon != null ? groupIcon.name : "null")}\n<color=silver>Group arguments:</color> {groupArgs}");
+        }
+    }
+}
+
+#endif
