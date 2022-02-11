1bb74b87ebb7bd7437f021ae84533e700c77e2df
Varneon <me@varneon.com>
2022-02-11 10:21:11 -0800

    Add SimplePlayerSettings prefab
    -Simple Player Settings is a simple, but more advanced replacement for the original "VRCWorldSettings" UdonBehaviour that ships with VRCSDK

---
+++ Assets/Varneon/Udon Prefabs/Essentials/Simple Player Settings/README.md
@@ -0,0 +1,10 @@
+Varneon's UdonEssentials - Simple Player Settings
+
+This prefab is a simple, but more advanced replacement for the original "VRCWorldSettings" UdonBehaviour that ships with VRCSDK
+
+You can simply set all of the parameters on the UdonBehaviour to whatever values you wish to be applied to every user in your world
+
+Check out the following VRChat documentation pages to understand all of the specific parameters:
+
+https://docs.vrchat.com/docs/player-forces
+https://docs.vrchat.com/docs/player-audio
---
+++ Assets/Varneon/Udon Prefabs/Essentials/Simple Player Settings/Udon Programs/SimplePlayerSettings.cs
@@ -0,0 +1,63 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace Varneon.UdonPrefabs.Essentials
+{
+    /// <summary>
+    /// More advanced but still extremely simple replacement for the original VRCSDK's "VRCWorldSettings" UdonBehaviour
+    /// </summary>
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class SimplePlayerSettings : UdonSharpBehaviour
+    {
+        [Header("Local Player Movement")]
+        [SerializeField] private float walkSpeed = 3f;
+        [SerializeField] private float runSpeed = 6f;
+        [SerializeField] private float strafeSpeedMultiplier = 1f;
+        [SerializeField] private float jumpImpulse = 3f;
+
+        [Space]
+        [Header("Remote Player Voice")]
+        [SerializeField] private float voiceGain = 15f;
+        [SerializeField] private float voiceDistanceNear = 0f;
+        [SerializeField] private float voiceDistanceFar = 25f;
+        [SerializeField] private float voiceVolumetricRadius = 0;
+        [SerializeField] private bool voiceLowpass = true;
+
+        [Space]
+        [Header("Remote Player Avatar Audio")]
+        [SerializeField] private float avatarAudioGain = 10f;
+        [SerializeField] private float avatarAudioDistanceNear = 40f;
+        [SerializeField] private float avatarAudioDistanceFar = 40f;
+        [SerializeField] private float avatarAudioVolumetricRadius = 0;
+        [SerializeField] private bool avatarAudioForceSpatial = true;
+        [SerializeField] private bool avatarAudioCustomCurve = true;
+
+        public override void OnPlayerJoined(VRCPlayerApi player)
+        {
+            if (player.isLocal)
+            {
+                player.SetWalkSpeed(walkSpeed);
+                player.SetRunSpeed(runSpeed);
+                player.SetStrafeSpeed(walkSpeed * strafeSpeedMultiplier);
+                player.SetJumpImpulse(jumpImpulse);
+            }
+            else
+            {
+                player.SetVoiceGain(voiceGain);
+                player.SetVoiceDistanceNear(voiceDistanceNear);
+                player.SetVoiceDistanceFar(voiceDistanceFar);
+                player.SetVoiceVolumetricRadius(voiceVolumetricRadius);
+                player.SetVoiceLowpass(voiceLowpass);
+
+                player.SetAvatarAudioGain(avatarAudioGain);
+                player.SetAvatarAudioNearRadius(avatarAudioDistanceNear);
+                player.SetAvatarAudioFarRadius(avatarAudioDistanceFar);
+                player.SetAvatarAudioVolumetricRadius(avatarAudioVolumetricRadius);
+                player.SetAvatarAudioForceSpatial(avatarAudioForceSpatial);
+                player.SetAvatarAudioCustomCurve(avatarAudioCustomCurve);
+            }
+        }
+    }
+}
