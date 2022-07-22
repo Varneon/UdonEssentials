deed7140eadec25eb95a24cf5a94129cabf115e9
Varneon <me@varneon.com>
2022-07-22 08:48:22 -0700

    Fix NullReferenceException on Noclip inspector
    After previewing the Noclip script through prefab override popup, OnDisable / OnDestroy would result in NullReferenceException because the custom inspector isn't used for it

--- Assets/Varneon/Udon Prefabs/Core/Noclip/Editor/NoclipEditor.cs
+++ Assets/Varneon/Udon Prefabs/Core/Noclip/Editor/NoclipEditor.cs
@@ -43,8 +43,11 @@ namespace Varneon.UdonPrefabs.Core.Editor
             APIDocumentationBuilder.BuildAPIDocumentation(root.Q<Foldout>("Foldout_API"), typeof(Noclip));
         }
 
-        private void OnDisable()
+        private void OnDestroy()
         {
+            // If foldouts is null, then OnDestroy was most likely called by prefab override preview
+            if(foldouts == null) { return; }
+
             int states = 0;
 
             for (int i = 0; i < foldouts.Count; i++)
