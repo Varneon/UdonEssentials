f51b13a74b8df67512027adfd8aa19b5be4711e7
Varneon <me@varneon.com>
2022-07-22 08:18:15 -0700

    Add NeonInspector
    NeonInspector is the default inspector wrapper for V-Inspector for the Udon prefabs

---
+++ Assets/Varneon/Udon Prefabs/NeonInspector/Editor/NeonInspector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+using Varneon.VInspector;
+
+namespace Varneon.UdonPrefabs
+{
+    public class NeonInspector : InspectorBase
+    {
+        [SerializeField]
+        private Texture2D banner;
+
+        protected override void OnInspectorVisualTreeAssetCloned(VisualElement root)
+        {
+            root.Q("Banner").style.backgroundImage = banner;
+        }
+    }
+}
