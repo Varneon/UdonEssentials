a7b76de26558968762e787c1bea7199b6296ed1a
Varneon <me@varneon.com>
2022-07-22 08:21:53 -0700

    Add EventDispatcher custom editor

---
+++ Assets/Varneon/Udon Prefabs/Core/Event Dispatcher/Editor/EventDispatcherEditor.cs
@@ -0,0 +1,28 @@
+﻿using UdonSharp;
+using UnityEditor;
+using UnityEngine.UIElements;
+using Varneon.VInspector;
+
+namespace Varneon.UdonPrefabs.Core.Editor
+{
+    /// <summary>
+    /// Custom inspector for EventDispatcher prefab
+    /// </summary>
+    [CustomEditor(typeof(EventDispatcher))]
+    [IgnoreFieldsOfType(typeof(UdonSharpBehaviour))]
+    public class EventDispatcherEditor : NeonInspector
+    {
+        protected override void OnInspectorVisualTreeAssetCloned(VisualElement root)
+        {
+            VisualElement inspectorPanel = root.Q("InspectorPanel");
+
+            Foldout apiFoldout = new Foldout() { name = "Foldout_API", text = "API", value = false };
+
+            inspectorPanel.Add(apiFoldout);
+
+            APIDocumentationBuilder.BuildAPIDocumentation(apiFoldout, typeof(EventDispatcher));
+
+            base.OnInspectorVisualTreeAssetCloned(root);
+        }
+    }
+}
