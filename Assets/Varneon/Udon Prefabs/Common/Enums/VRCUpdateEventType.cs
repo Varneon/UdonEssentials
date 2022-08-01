2d091f25904323cb745a81c34b1dcf700ad3d01c
Varneon <me@varneon.com>
2022-08-01 03:14:09 -0700

    Add public enum for all of VRChat's update events

---
+++ Assets/Varneon/Udon Prefabs/Common/Enums/VRCUpdateEventType.cs
@@ -0,0 +1,25 @@
+﻿namespace Varneon.UdonPrefabs.Common.VRCEnums
+{
+    /// <summary>
+    /// The types of update events that can be used in VRChat
+    /// </summary>
+    public enum VRCUpdateEventType
+    {
+        /// <summary>
+        /// Unity Fixed Update
+        /// </summary>
+        FixedUpdate,
+        /// <summary>
+        /// Unity Update
+        /// </summary>
+        Update,
+        /// <summary>
+        /// Unity Late Update
+        /// </summary>
+        LateUpdate,
+        /// <summary>
+        /// VRChat's Post-Late Update
+        /// </summary>
+        PostLateUpdate
+    }
+}
